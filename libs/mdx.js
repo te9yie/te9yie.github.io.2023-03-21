d0b68e01cc9655a5167df015778e8f2c4acc3f38
te9yie <te9yie@users.noreply.github.com>
2021-04-04 15:15:52 +0900

    Add CodeBlock

--- libs/mdx.js
+++ libs/mdx.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import CodeBlock from "../components/CodeBlock";
 import remarkWikiLink from "remark-wiki-link";
 
 export const components = {
   a: (props) => <Link {...props} />,
+  code: CodeBlock,
 };
 
 export const remarkPlugins = [
