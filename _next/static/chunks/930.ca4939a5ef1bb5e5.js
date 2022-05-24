f0f3cae7aab5de67a6553273bee519d588d48217
te9yie <te9yie@users.noreply.github.com>
2022-05-24 19:10:35 +0000

    deploy: 8b71e03b50fc20fb9b3e5ea91c07cc5521167dea

---
+++ _next/static/chunks/930.ca4939a5ef1bb5e5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[930],{1930:function(e,t,u){u.r(t);var n=180;t.default={setup:function(e,t){e.createCanvas(320,n).parent(t),e.rectMode(e.CENTER),e.noStroke()},draw:function(e){var t=e.mouseY-90;e.background(222),e.fill("#e60033"),e.rect(320-e.mouseX,90,80-t,80-t),e.rect(e.mouseX,90,80+t,80+t)}}}}]);
