c11b8d20c27440555df0596b237f82391b982753
te9yie <te9yie@users.noreply.github.com>
2022-05-27 10:49:11 +0900

    update

--- libs/p5/shader00.js
+++ libs/p5/shader00.js
@@ -6,17 +6,61 @@ precision mediump float;
 attribute vec3 aPosition;
 
 void main() {
-    gl_Position = vec4(aPosition, 1.0);
+  gl_Position = vec4(aPosition, 1.0);
 }`;
 const FRAG = `
 precision mediump float;
 uniform float uRate;
 uniform vec2 uResolution;
 
+const float PI = 3.14159265;
+const float angle = 60.0;
+const float fov = angle * 0.5 * PI / 180.0;
+const vec3 light_dir = normalize(vec3(-1, 1, 1));
+
+vec3 trans(vec3 p) {
+  return mod(p, 3.0) - 1.5;
+}
+
+float sphere(vec3 p, float r) {
+  return length(trans(p)) - r;
+}
+
+float calc(vec3 p) {
+  return sphere(p, 1.0);
+}
+
+vec3 calc_normal(vec3 p) {
+  float d = 0.0001;
+  return normalize(vec3(
+    calc(p + vec3(d, 0, 0)) - calc(p - vec3(d, 0, 0)),
+    calc(p + vec3(0, d, 0)) - calc(p - vec3(0, d, 0)),
+    calc(p + vec3(0, 0, d)) - calc(p - vec3(0, 0, d))));
+}
+
 void main() {
-    float x = gl_FragCoord.x / uResolution.x * uRate;
-    float y = gl_FragCoord.y / uResolution.y * (1.0 - uRate);
-    gl_FragColor = vec4(x, y, 0, 1);
+  vec2 p = (gl_FragCoord.xy * 2.0 - uResolution) / min(uResolution.x, uResolution.y);
+  vec3 eye = vec3(sin(uRate) * 0.5, cos(uRate) * 0.5, 3);
+  vec3 ray = normalize(vec3(sin(fov) * p.x, sin(fov) * p.y, -cos(fov)));
+
+  // march
+  float distance = 0.0;
+  float len = 0.0;
+  vec3 pos = eye;
+  for (int i = 0; i < 64; ++i) {
+    distance = calc(pos);
+    len += distance;
+    pos = eye + ray * len;
+  }
+
+  if (abs(distance) < 0.001) {
+    vec3 n = calc_normal(pos);
+    //gl_FragColor = vec4(n, 1);
+    float diff = clamp(dot(light_dir, n), 0.1, 1.0);
+    gl_FragColor = vec4(vec3(diff), 1);
+  } else {
+    gl_FragColor = vec4(vec3(0), 1);
+  }
 }`;
 
 let a_shader;
